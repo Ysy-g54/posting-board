89d111d7c8a2932a278332e7fde32816cab8a5a1
Yoshiya-g <y.bskmelo7@gmail.com>
2020-03-10 00:55:52 +0900

    ログインコンポーネントのみ実装

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="text-center">
+    <v-bottom-sheet v-model="sheet" persistent>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="green"
+          dark
+          v-on="on"
+        >
+          Open Persistent
+        </v-btn>
+      </template>
+      <v-sheet class="text-center" height="200px">
+        <v-btn
+          class="mt-6"
+          flat
+          color="error"
+          @click="sheet = !sheet"
+        >close</v-btn>
+        <div class="py-3">This is a bottom sheet using the persistent prop</div>
+      </v-sheet>
+    </v-bottom-sheet>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data: () => ({
+	sheet: false,
+  })
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Login from "@/components/login/Login";
 
 Vue.use(Router);
 
 const router = new Router({
-  routes: [
-
-  ]
+	routes: [
+		{
+			path: "/login",
+			name: "login",
+			component: Login,
+			meta: { isPublic: true, title: "ログイン" }
+		}
+	]
 });
 
 // router.beforeEach((to, from, next) => {
