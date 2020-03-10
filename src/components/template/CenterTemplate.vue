f08ab5c2c75210fecd8103715e7e092cd286aa05
yoshiya.gibo <[yoshiya.gibo@persol.co.jp]>
2020-03-10 19:58:39 +0900

    add漏れcommmit

---
+++ src/components/template/CenterTemplate.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <slot></slot>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: "center-template",
+  data: () => ({})
+};
+</script>
