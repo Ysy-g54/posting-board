c2226ee38543c23aa52f9c10cc169e6cb1096f18
Yoshiya-g <y.bskmelo7@gmail.com>
2020-04-09 22:09:46 +0900

    refs

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+export default {
+	apiKey: "AIzaSyACCHfqnglByenqNwbeXTP5_yq-b0zGY1s",
+	authDomain: "posting-board-test.firebaseapp.com",
+	databaseURL: "https://posting-board-test.firebaseio.com",
+	projectId: "posting-board-test",
+	storageBucket: "posting-board-test.appspot.com",
+	messagingSenderId: "786794730001",
+	appId: "1:786794730001:web:f3a6f6d20c1066860897b0",
+	measurementId: "G-L2SPXJVQW5"
+};
