21a632fb8cba01105dc057b8c7beabc3b4d78654
Yoshiya-g <y.bskmelo7@gmail.com>
2020-04-09 15:02:28 +0900

    refs

--- src/service/database.js
+++ src/service/database.js
@@ -2,14 +2,10 @@ import Vue from "vue";
 import { firestorePlugin } from 'vuefire';
 import firebase from "firebase/app";
 import "firebase/firestore";
+import firebaseConfig from '@/configs/firebase.js';
 
 Vue.use(firestorePlugin);
-// let database = null;
-// fetch('/__/firebase/init.json').then(async response => {
-// 	console.error(await response, response.json());
-// 	firebase.initializeApp(await response);
+firebase.initializeApp(firebaseConfig);
 
-// });
 const database = firebase.firestore();
 export default database;
-
