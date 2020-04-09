e6bf6d3c2a8f71d799a227974a70372e4edbbd13
Yoshiya-g <y.bskmelo7@gmail.com>
2020-04-09 15:07:05 +0900

    refs

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,10 +1,10 @@
 export default {
-	apiKey: "×××",
-	authDomain: "×××",
-	databaseURL: "×××",
-	projectId: "×××",
-	storageBucket: "×××",
-	messagingSenderId: "×××",
-	appId: "×××",
-	measurementId: "×××"
+	apiKey: "AIzaSyACCHfqnglByenqNwbeXTP5_yq-b0zGY1s",
+	authDomain: "posting-board-test.firebaseapp.com",
+	databaseURL: "https://posting-board-test.firebaseio.com",
+	projectId: "posting-board-test",
+	storageBucket: "posting-board-test.appspot.com",
+	messagingSenderId: "786794730001",
+	appId: "1:786794730001:web:f3a6f6d20c1066860897b0",
+	measurementId: "G-L2SPXJVQW5"
 };
