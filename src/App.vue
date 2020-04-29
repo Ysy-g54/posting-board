1930db7e33c326f0118df684b499ec8b8e441546
Yoshiya-g <y.bskmelo7@gmail.com>
2020-04-29 16:53:20 +0900

    ルーティングのstateを管理するように修正 #23

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <v-app>
-    <router-view></router-view>
-  </v-app>
+  <div>
+    <v-app v-if="getIsLoading">
+      <v-layout align-center justify-center>
+        <v-progress-circular :size="200" :width="7" color="primary" indeterminate></v-progress-circular>
+      </v-layout>
+    </v-app>
+    <v-app v-else>
+      <router-view></router-view>
+    </v-app>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: "App"
+  name: "App",
+  computed: {
+    ...mapGetters(["getIsLoading"])
+  }
 };
 </script>
 
